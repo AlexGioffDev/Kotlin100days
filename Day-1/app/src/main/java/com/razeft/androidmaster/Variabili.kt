64f5619aa61b8cbf6c3fcebc4a746d8e726e6229
AlexGioffDev <alex.gioffre_dev@outlook.it>
2024-06-02 19:20:13 +0200

    Day1: Variabili

---
+++ Day-1/app/src/main/java/com/razeft/androidmaster/Variabili.kt
@@ -0,0 +1,22 @@
+package com.razeft.androidmaster
+
+fun main(){
+    // Numeri
+    val age: Int = 32
+    val longValue: Long = 30
+    val floatExample: Float = 32.2f
+    val doubleExample: Double = 42.2
+
+    // Alfanumeriche
+    var letra: Char = 'a'
+    var otraLetra: Char = '2'
+    var nueva: Char = '@'
+
+    val name: String = "Razeft"
+
+    // Boolean
+    val verdadero: Boolean = true
+    var falsero: Boolean = false
+
+    print(doubleExample)
+}
